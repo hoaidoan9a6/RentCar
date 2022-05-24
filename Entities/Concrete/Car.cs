cfef79493400f11e9681388b8aac7c133db325de
hoaidoan9a6 <hoaidoan9a6@gmail.com>
2022-05-25 04:49:53 +0900

    first commit

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Core;
+
+namespace Entities.Concrete
+{
+    public interface Brand : IEntity
+    {
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+    }
+}
---
+++ RentCar/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RentCar
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
