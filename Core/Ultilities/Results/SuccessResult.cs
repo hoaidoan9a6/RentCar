9d28860d1fa598798371e3ff6bfd0b5bcaf6eb51
hoaidoan <hoaidoan916@gmail.com>
2022-05-25 11:57:07 +0900

    #13 Complete Result with Success

---
+++ Core/Ultilities/Results/SuccessResult.cs
@@ -0,0 +1,16 @@
+namespace Core.Ultilities.Results
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult(string message):base(true, message)
+        {
+
+        }
+
+        public SuccessResult():base(true)
+        {
+
+        }
+
+    }
+}
